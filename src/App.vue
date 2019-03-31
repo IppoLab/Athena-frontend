b3fba45073dd40535931e8e07184ba7defdada70
Sidnev Nikolaj <nsidnev@ccsteam.ru>
2019-03-31 15:29:01 +0300

    fix cypress base test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <div id="app">
-    <v-app dark=""><router-view></router-view></v-app>
-  </div>
+    <div id="app">
+        <v-app dark>
+            <router-view></router-view>
+            <v-footer app>
+                <v-layout align-end justify-end>
+                    <span class="ma-4">Still In Development</span>
+                </v-layout>
+            </v-footer>
+        </v-app>
+    </div>
 </template>
 
