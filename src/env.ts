0f235eaaee8d50beb5e3834456e13c6118e1ed89
Sidnev Nikolaj <nsidnev@ccsteam.ru>
2019-04-21 23:47:45 +0300

    fix secure env parsing

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,8 @@ let envApiUrl = '';
 if (env === 'production') {
     envApiUrl = `https://${process.env.VUE_APP_DOMAIN_PROD}`;
 } else {
-    const secureConnection = JSON.parse(process.env.VUE_APP_DOMAIN_DEV_SECURE);
+    const httpsEnv = process.env.VUE_APP_DOMAIN_DEV_SECURE;
+    const secureConnection = httpsEnv !== undefined ? JSON.parse(httpsEnv) : false;
     const scheme = secureConnection ? 'https' : 'http';
     envApiUrl = `${scheme}://${process.env.VUE_APP_DOMAIN_DEV}`;
 }
